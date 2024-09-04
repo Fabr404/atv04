f924313137b448586bfee96e9292fa19c95adb48
fabr404 <fabriciofelixsilva11@gmail.com>
2024-09-04 15:59:09 -0300

    atv 04

---
+++ atv_04.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+n = float(input("nota 01 : "))
+n1 = float(input("nota 02 : "))
+n2 = float(input("nota 03 : "))
+s = (n+n1+n2)/3
+media = s
+print(media)
